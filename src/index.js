e539ec27e830ef9a8864ec2c4fec8ee2dd9cfd83
tlin108 <tlin108@hotmail.com>
2016-12-09 17:22:16 -0500

    added component file structure and youtube api key

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const API_KEY = 'AIzaSyC_EC6--BQRYxKce45S98jB5sl9IZHskbY';
+
+const App = () => {
+  return <div>Hello World!</div>
+}
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
