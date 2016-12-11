18113a25fad0502bd82e4d647a60cedc961054b8
tlin108 <tlin108@hotmail.com>
2016-12-11 13:01:40 -0500

    add search bar component

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import SearchBar from './components/SearchBar';
+
 const API_KEY = 'AIzaSyC_EC6--BQRYxKce45S98jB5sl9IZHskbY';
 
 function App() {
-  return <div>Hello World!</div>
+  return (
+    <div>
+      <SearchBar />
+    </div>
+  );
 }
 
 ReactDOM.render(
