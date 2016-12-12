2533896486d24492a94045faf4552597370a125e
tlin108 <tlin108@hotmail.com>
2016-12-11 21:54:29 -0500

    refactor index.js into class component

--- src/index.js
+++ src/index.js
@@ -1,16 +1,29 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
 
 import SearchBar from './components/SearchBar';
 
 const API_KEY = 'AIzaSyC_EC6--BQRYxKce45S98jB5sl9IZHskbY';
 
-function App() {
-  return (
-    <div>
-      <SearchBar />
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+  
+    this.state = { videos: [] };
+
+    YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+      this.setState({ videos });
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <SearchBar />
+      </div>
+    );
+  }
 }
 
 ReactDOM.render(
