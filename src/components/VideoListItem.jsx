7d99cb695e0a5153a8335bf2518117b5dd568460
tlin108 <tlin108@hotmail.com>
2016-12-12 23:10:46 -0500

    add video selection

--- src/components/VideoListItem.jsx
+++ src/components/VideoListItem.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 
-function VideoListItem({video}) {
+function VideoListItem({video, onVideoSelect}) {
   const imageUrl = video.snippet.thumbnails.default.url;
 
   return (
-    <li className="list-group-item">
+    <li onClick={() => onVideoSelect(video)} className="list-group-item">
       <div className="video-list media">
         <div className="media-left">
           <img className="media-object" src={imageUrl}/>
